e9745b3116880bdc8a681981bf0c4c3250c721ce
Johan Santallana <johan921229@gmail.com>
2025-08-10 21:23:21 -0400

    subiendo JS basico

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-import "bootstrap";
-import "./style.css";
-
 window.onload = function() {
   //write your code here
+  let cardType = ["♠","♥", "♦", "♣"];
+  let numCard = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let type = cardType[Math.floor(Math.random()*cardType.length)];
+
+  document.getElementById("top").innerHTML = type;
+  document.getElementById("bottom").innerHTML = type;
+  document.getElementById("center").innerHTML = numCard[Math.floor(Math.random()*numCard.length)];
   
 };
