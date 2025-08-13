9e3dc2f2c24fd226c47170f31b0d8d69aa17fb2b
Johan Santallana <johan921229@gmail.com>
2025-08-12 20:54:02 -0400

    añadiendo condicion de color rojo para los corazones y los rombos

--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@ window.onload = function() {
   let numCard = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
   let type = cardType[Math.floor(Math.random()*cardType.length)];
+  let num = numCard[Math.floor(Math.random()*numCard.length)];
+  
 
   document.getElementById("top").innerHTML = type;
   document.getElementById("bottom").innerHTML = type;
-  document.getElementById("center").innerHTML = numCard[Math.floor(Math.random()*numCard.length)];
+  document.getElementById("center").innerHTML = num;
+
+  if (type === "♥" || type === "♦") {
+    document.getElementById("top").style.color = "red";
+    document.getElementById("bottom").style.color = "red";
+    document.getElementById("center").style.color = "red";
+  }
   
 };
